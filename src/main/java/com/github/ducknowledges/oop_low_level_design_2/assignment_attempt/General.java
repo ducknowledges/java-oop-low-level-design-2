ad49306d5498aad0fbfe6416f068e3f8c89e7d99
Oleg Kononov <kononov.itwork@gmail.com>
2025-05-11 18:16:02 +0400

    feat: add assignment_attempt

---
+++ src/main/java/com/github/ducknowledges/oop_low_level_design_2/assignment_attempt/General.java
@@ -0,0 +1,19 @@
+package com.github.ducknowledges.oop_low_level_design_2.assignment_attempt;
+
+import java.io.Serializable;
+
+class General implements Serializable {
+
+    @SuppressWarnings("unchecked")
+    public static <T extends General> T assignmentAttempt(Object source, Class<T> targetClass) {
+        if (!targetClass.isInstance(source)) {
+            return (T) new None();
+        }
+        return (T) source;
+    }
+
+}
+
+class Any extends General { }
+
+final class None extends Any /*A, B, ....*/ { }
